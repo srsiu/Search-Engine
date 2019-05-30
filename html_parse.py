098c4ddb7a8215c6b3881b56cc4fdc4fd4e31ef7
srsiu <34078992+srsiu@users.noreply.github.com>
2019-05-30 02:55:42 -0700

    Add files via upload

--- html_parse.py
+++ html_parse.py
@@ -3,10 +3,45 @@ import lxml.html
 import lxml.etree
 import json
 import os
+import re
 from collections import defaultdict
 from bs4 import BeautifulSoup
 from urllib.parse import urlparse
 
+def rem(Li:list)->str:
+	'''splits the input if its an alphanum, and removes all the extra symbols, returns a list
+	'''
+	'''Complexity: O(N) since it there is only one loop, and splitting the string and checking if its alphanum'''
+	pat ="[a-zA-Z0-9']+"
+	L = re.findall(pat,Li)
+	return L
+def make_dict(L:list)-> dict:
+	'''makes a dictionary with a key of the word and value of the number of occurrences
+		returns the dictionary
+	'''
+	'''Complexity: O(N) since it there is only one loop looping through the list'''
+	d={}
+	for word in L:
+		if word not in d:
+			d[word]=1
+		else:
+			d[word]+=1	
+	return d
+def term_freq(total_string):
+    token_list = rem(total_string)
+    t_freq = make_dict(token_list)
+    return t_freq
+    
+def create_index(tf, folder, invert_ind):
+    for term in tf:
+        print(type(term))
+        invert_ind["Slide"] =5
+        if term in invert_ind:
+            invert_ind[term].append({"freq":tf[term], "docID":folder})
+        else:
+            invert_ind[term] = [{"freq":tf[term], "docID":folder}]
+        
+        
 
 def html_parse():
     file_path = os.path.join('.', 'WEBPAGES_RAW', 'bookkeeping.json')
@@ -19,6 +54,8 @@ def html_parse():
     
     '''docs is a list of set of words in each doc'''
     docs = list()
+    '''i think its dictionary intialization'''
+    invert_ind = defaultdict(list)
     for folder in webpage_dict:
         address = folder.split("/")
         dir = address[0]
@@ -29,8 +66,15 @@ def html_parse():
             with open(file_name, "r") as html_doc:
                 soup = BeautifulSoup(html_doc, "lxml")
                 paragraphs = soup.find_all('p')
+                total_string = ""
                 for p in paragraphs:
-                    print(p.text)
+                    total_string += p.text
+                tf = term_freq(total_string)
+                print(tf)
+                invert_ind = create_index(tf, folder,invert_ind)
+                #for x,y in invert_ind:
+                #print(x,"\t",y)
+                
                 
                     
 """write main function"""
