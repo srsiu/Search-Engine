815fc9e2ce9c6bcdcc8ed17d3df4edede6c94518
EuniceT <eunice.trinh@gmail.com>
2019-05-31 12:43:44 -0700

    Updates

--- Query.py
+++ Query.py
@@ -1,8 +1,29 @@
 import nltk
+import re
 
 class Query:
 	def __init__(self, query):
 		self.query = query
+		self.term_f = {}
+		self.tokens = []
 
 	def create_tokens(self):
-		tokens = nltk.word_tokenize(query)
+		self.query.strip()
+		self.query = self.query.lower()
+		self.query = re.sub(r'[^a-zA-Z0-9 ]+', '', self.query)
+		self.tokens = nltk.word_tokenize(self.query)
+
+	def get_freq(self):
+		for word in self.tokens:
+			if word in self.term_f:
+				self.term_f[word] += 1
+			else:
+				self.term_f[word] = 1
+
+	def run_query(self):
+		self.create_tokens()
+		self.get_freq()
+
+		#for w in self.term_f:
+		#	print(w, self.term_f[w])
+
